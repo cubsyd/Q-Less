73dbceeeb05faccdb6556ae2e75947de67048dc6
cubsyd <cubsherrera@gmail.com>
2026-06-12 00:58:37 -0500

    upd_023_backend_actualizado

--- backend/laravel_backend/backend_Q-LESS/app/Http/Kernel.php
+++ backend/laravel_backend/backend_Q-LESS/app/Http/Kernel.php
@@ -7,8 +7,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \App\Http\Middleware\ForceCorsHeaders::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
